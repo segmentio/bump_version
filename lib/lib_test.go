73fe0ada180473691058e5575e031df3fd00b17d
Kevin Burke <kevin@burke.dev>
2019-09-17 10:17:09 -0700

    all: fix tests

--- lib/lib_test.go
+++ lib/lib_test.go
@@ -1,6 +1,9 @@
 package bump_version
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestChangeVersion(t *testing.T) {
 	testCases := []struct {
@@ -22,5 +25,12 @@ func TestChangeVersion(t *testing.T) {
 		if v.String() != tt.out {
 			t.Errorf("changeVersion(%s, %s): got %s, want %s", tt.vtype, tt.in, v.String(), tt.out)
 		}
+	}
+}
+
+func TestVersionString(t *testing.T) {
+	typ := reflect.TypeOf(VERSION)
+	if typ.String() != "string" {
+		t.Errorf("expected VERSION to be a string, got %#v (type %#v)", VERSION, typ.String())
 	}
 }
--- main_test.go
+++ main_test.go
@@ -1,13 +1,1 @@
 package main
-
-import (
-	"reflect"
-	"testing"
-)
-
-func TestVersionString(t *testing.T) {
-	typ := reflect.TypeOf(VERSION)
-	if typ.String() != "string" {
-		t.Errorf("expected VERSION to be a string, got %#v (type %#v)", VERSION, typ.String())
-	}
-}
